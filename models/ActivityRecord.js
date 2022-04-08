5d26e66e460df3b488248fb4c7236cc95def6b6a
newstar <newstar0207@naver.com>
2022-04-08 13:00:25 +0900

    feat: 경사도 추가

--- models/ActivityRecord.js
+++ models/ActivityRecord.js
@@ -1,16 +1,19 @@
 class ActivityRecord {
-  constructor({
-    trackId,
-    name,
-    userId,
-    gps,
-    speed,
-    time,
-    totalTime,
-    distance,
-    event,
-    altitude,
-  }) {
+  constructor(
+    {
+      trackId,
+      name,
+      userId,
+      gps,
+      speed,
+      time,
+      totalTime,
+      distance,
+      event,
+      altitude,
+    },
+    slope
+  ) {
     this.trackId = trackId;
     this.user = { userId, name };
     this.gps = { coordinates: gps };
@@ -20,6 +23,8 @@ class ActivityRecord {
     this.distance = distance;
     this.event = event;
     this.altitude = altitude;
+    this.slope = slope;
+    this.avgSlope = slope.reduce((sum, item) => sum + item, 0) / slope.length;
   }
 }
 
