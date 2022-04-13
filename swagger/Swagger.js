3fee125b36c959c62a3f38b15a0e6c0470ad9469
newstar <newstar0207@naver.com>
2022-04-13 14:57:57 +0900

    docs: swagger response 형식 변경

--- swagger/Swagger.js
+++ swagger/Swagger.js
@@ -1,22 +1,21 @@
-const swaggerUi = require('swagger-ui-express'); 
-const swaggereJsdoc = require('swagger-jsdoc');
+const swaggerUi = require("swagger-ui-express");
+const swaggereJsdoc = require("swagger-jsdoc");
 
 const options = {
-    swaggerDefinition: {
-        components: {},
-        openapi: "3.0.0",
-        info: {
-            title: 'mongoDB API',
-            version: '1.0.0', 
-            description: 'Track, GPSdata with mongo', 
-        }, 
-        host: 'localhost:3002', 
-        basePath: '/api' 
-    }, 
-    apis: ['./routes/*.js', './swagger/*'] 
-}; 
+  swaggerDefinition: {
+    components: {},
+    openapi: "3.0.0",
+    info: {
+      title: "mongoDB API",
+      version: "1.0.0",
+      description: "Track, GPSdata with mongo",
+    },
+    host: "localhost:3002",
+    basePath: "/api",
+  },
+  apis: ["./routes/*.js", "./swagger/*"],
+};
 
-const specs = swaggereJsdoc(options); 
+const specs = swaggereJsdoc(options);
 
 module.exports = { swaggerUi, specs };
-
